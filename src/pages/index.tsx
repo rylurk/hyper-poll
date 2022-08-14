3cc2e579204c3d284a428a134dc09be43c616538
ryan <ryanslh5@gmail.com>
2022-08-14 10:15:47 -0400

    Add tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import { prisma } from '../db/client';
+import { trpc } from '../utils/trpc';
 
 export default function HomePage(props: any) {
+  const { data, isLoading } = trpc.useQuery(['hello', { text: 'ryan' }]);
+
+  if (isLoading || !data) return <div>Loading...</div>;
+
+  return <div>{data.greeting}</div>;
+
   return (
     <div>
       <h1>Hello</h1>
