4ab0b958609dbc0270af871c95f4638f9d6478ad
ryan <ryanslh5@gmail.com>
2022-08-15 12:20:17 -0400

    restructure backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { prisma } from '../db/client';
 import { trpc } from '../utils/trpc';
 
 export default function HomePage(props: any) {
-  const { data, isLoading } = trpc.useQuery(['getAllQuestions']);
+  const { data, isLoading } = trpc.useQuery(['questions.get-all']);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
