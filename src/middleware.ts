3cc04d5d50a902913905e3f09c27cab728d008c1
ryan <lurkair@gmail.com>
2022-08-22 14:27:02 -0400

    add cookie creation on initial request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { nanoid } from 'nanoid';
 
 export function middleware(req: NextRequest) {
-  if (req.cookies.get('userCookie')) return;
+  if (req.cookies.get('poll-token')) return;
 
-  const random = Math.random().toString();
+  const random = nanoid();
   console.log('Request cookies', req.cookies);
 
   const res = NextResponse.next();
-  res.cookies.set('userCookie', random, { sameSite: 'strict' });
+  res.cookies.set('poll-token', random, { sameSite: 'strict' });
 
   return res;
 }
