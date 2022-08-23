96e250c960e9c31f8e698930f0b944f72e28a157
ryan <lurkair@gmail.com>
2022-08-23 18:45:04 -0400

    filter questions by ones user asked

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const QuestionCreator: React.FC = () => {
   const client = trpc.useContext();
   const { mutate, isLoading } = trpc.useMutation('questions.create', {
     onSuccess: () => {
-      client.invalidateQueries('questions.get-all');
+      client.invalidateQueries('questions.get-all-my-questions');
       if (!inputRef.current) return;
       inputRef.current.value = '';
     },
@@ -28,7 +28,7 @@ const QuestionCreator: React.FC = () => {
 };
 
 export default function HomePage() {
-  const { data, isLoading } = trpc.useQuery(['questions.get-all']);
+  const { data, isLoading } = trpc.useQuery(['questions.get-all-my-questions']);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
